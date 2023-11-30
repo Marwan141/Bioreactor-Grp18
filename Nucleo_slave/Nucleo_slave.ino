66431c6c0cb4722051f8255d5a8c3087065beba9
Marwan <yassinimarwan6@gmail.com>
2023-11-30 16:09:01 +0000

    added dual way stuff for Nucleo Slave

--- Nucleo_slave/Nucleo_slave.ino
+++ Nucleo_slave/Nucleo_slave.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 
-#define SLAVE_ADDR 9
+#define SLAVE_ADDR 12
+#define MASTER_ADDR 11
+// #define I2C_SDA smth
+// #define I2C_SCL smth
 
 int my_delay = 1000;
 
@@ -8,15 +11,26 @@ void setup() {
   //Serial.begin(9600);
   Wire.begin(SLAVE_ADDR); // join I2C bus with slave address
   Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
 }
 
 void loop() {
   delay(my_delay);
-  Wire.onRequest(requestEvent); // register event
 }
 
 void requestEvent() {
   // Wire.write("T 257 "); // respond with message
   // Wire.write("P 57  ");
   Wire.write("R 1125");
+}
+
+void receiveEvent(int howMany) {
+
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+  }
+  Serial.println();
+
 }
